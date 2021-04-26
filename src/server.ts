a7126ba14b7244429db9559f7d4f32c90bbffa41
Leonardo Valverde RadarFit <70704988+leonardovalverderadarfit@users.noreply.github.com>
2021-04-26 02:10:29 -0300

    feat: sqlite structure

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({
-  message: "Olá NLW 05"
-  });
-});
+app.use(express.json());
 
-app.post("/", (resquest, response)=>{
-  return response.json({message: "Usuário salvo com sucesso"});
-});
+app.use(routes);
 
 app.listen(3333, ()=> console.log("Server is runing on port 3333"));
